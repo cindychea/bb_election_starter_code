47ac145f2bf2c3bea95bfdd1f3e25b26fb1615c5
cindychea <cindy_chea@hotmail.com>
2019-06-20 13:48:32 -0400

    completed part 1 of exercise

--- bbelection/static/js/main.js
+++ bbelection/static/js/main.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+    axios.get('https://bb-election-api.herokuapp.com/')
+        .then(response => {
+            let data = response.data.candidates;
+            const candidateList = document.getElementById('candidates');
+            data.forEach(function(candidate) {
+                let candidateInfo = document.createElement('li');
+                candidateInfo.innerText = candidate.name + '\n' + 'Number of Votes: ' + candidate.votes
+                candidateList.appendChild(candidateInfo)
+            })
+        });
+        
 });
